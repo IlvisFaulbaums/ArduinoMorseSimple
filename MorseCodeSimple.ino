a3cde22e2983d1269060a6f68aec5e810e656809
IlvisFaulbaums <48957213+IlvisFaulbaums@users.noreply.github.com>
2021-07-25 12:54:00 +0300

    Added lines for numbers

--- MorseCodeSimple.ino
+++ MorseCodeSimple.ino
@@ -4,7 +4,9 @@ int i = 0;
 String inputText = "";
 char lettersSmall[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 char lettersBig[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+char numbers[] = {'0','1','2','3','4', '5', '6', '7','8','9'};
 String lettersMorse[] = {"*-","-***","-*-*","-**","*","**-*","--*","****","**","*---","-*-","*-**","--","-*","---","*--*","--*-","*-*","***","-","**-","***-","*--","-**-","-*--","--**"};
+String numbersMorse[] = {"-----","*----","**---","***--","****-","*****","-****", "--***", "---**", "----*"};
 void setup() {
 pinMode(led_pin,OUTPUT);
 Serial.begin(9600);
@@ -20,9 +22,15 @@ void loop()
     for (int j = 0; j < inputText.length(); j++) {
       for (int i = 0; i < 26; i++) {
         if(inputText.charAt(j)==lettersSmall[i]||inputText.charAt(j)==lettersBig[i]){
-          Serial.println(lettersMorse[i]);
+            Serial.println(lettersMorse[i]);
             convertToMorse(lettersMorse[i]);
             }
+        }
+        for (int n = 0; n < 9; n++){
+                if(inputText.charAt(j)==numbers[n]){
+          Serial.println(numbersMorse[n]);
+          convertToMorse(numbersMorse[n]);
+                }
         }
     }    
 }
@@ -45,9 +53,7 @@ void convertToMorse(String string) {
     }        
 }
 
-void
-
-writeMorse(int d){
+void writeMorse(int d){
 //  Serial.println(d);
   digitalWrite(led_pin,HIGH);
   delay(d);
